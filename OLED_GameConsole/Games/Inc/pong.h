ef97a440a4c599a947501d86128c5874e8d81f12
Leonid98 <leonid98@ut.ee>
2023-01-29 19:48:21 +0200

    Pong refactored

--- OLED_GameConsole/Games/Inc/pong.h
+++ OLED_GameConsole/Games/Inc/pong.h
@@ -11,12 +11,17 @@
 #include "ssd1306.h"
 #include "stdbool.h"
 
+#define PONG_FIELD_X0       5
+#define PONG_FIELD_Y0       10
+#define PONG_FIELD_X1       SSD1306_WIDTH - 6
+#define PONG_FIELD_Y1       SSD1306_HEIGHT - 1
 
-#define PONG_FIELD_X0 5
-#define PONG_FIELD_Y0 10
-#define PONG_FIELD_X1 SSD1306_WIDTH - 6
-#define PONG_FIELD_Y1 SSD1306_HEIGHT - 1
-// Player paddle structure
+#define PONG_SCORE_X        35
+#define PONG_BALL_X0        44
+#define PONG_BALL_X1        84
+#define PONG_BALL_Y0        22
+#define PONG_BALL_Y1        42
+
 struct paddle
 {
     uint8_t x;
@@ -26,13 +31,12 @@ struct paddle
     bool isLost;
 };
 
-// Ball structure
 struct ball
 {
     uint8_t x;
     uint8_t y;
-    int8_t xVelocity;
-    int8_t yVelocity;
+    int8_t x_velocity;
+    int8_t y_velocity;
     uint8_t size;
 };
 
@@ -42,9 +46,6 @@ enum
     Paddle_Down
 };
 
-void pong_moveBall(struct ball *ball);
-void pong_movePaddle(struct paddle *paddle, uint8_t direction);
-void pong_checkForCollision(void);
 void pong_gameloop(void);
 
 #endif
--- OLED_GameConsole/Games/Src/pong.c
+++ OLED_GameConsole/Games/Src/pong.c
@@ -3,30 +3,37 @@
 #include "button.h"
 #include "joystcik.h"
 #include "timer_tick.h"
+#include "random.h"
 
-struct paddle player1 = {0, 20, 4, 16};
-struct paddle player2 = {123, 25, 4, 16};
-struct ball ball = {64, 32, 1, 1, 1};
+static void priv_moveBall(struct ball *ball);
+static void priv_movePaddle(struct paddle *paddle, uint8_t direction);
+static void priv_displayScore(uint16_t score);
+static priv_initGame(struct ball *ball, struct paddle *player1, struct paddle *player2);
 
-// Function to move the ball
-void pong_moveBall(struct ball *ball)
+static void priv_moveBall(struct ball *ball)
 {
-    ball->x += ball->xVelocity;
-    ball->y += ball->yVelocity;
+    ssd1306_FillRectangle(ball->x, ball->y, ball->x + ball->size, ball->y + ball->size, Black);
+
+    ball->x += ball->x_velocity;
+    ball->y += ball->y_velocity;
 
     if (ball->y + ball->size >= PONG_FIELD_Y1 || ball->y <= PONG_FIELD_Y0)
     {
-        ball->yVelocity = -ball->yVelocity;
+        ball->y_velocity = -ball->y_velocity;
     }
 
     if (ball->x + ball->size >= PONG_FIELD_X1 || ball->x <= PONG_FIELD_X0)
     {
-        ball->xVelocity = -ball->xVelocity;
+        ball->x_velocity = -ball->x_velocity;
     }
+
+    ssd1306_FillRectangle(ball->x, ball->y, ball->x + ball->size, ball->y + ball->size, White);
 }
 
-void pong_movePaddle(struct paddle *paddle, uint8_t direction)
+static void priv_movePaddle(struct paddle *paddle, uint8_t direction)
 {
+    ssd1306_FillRectangle(paddle->x, paddle->y, paddle->x + paddle->width, paddle->y + paddle->height, Black);
+
     if (direction == Paddle_Down && (paddle->y + paddle->height) < PONG_FIELD_Y1)
     {
         paddle->y++;
@@ -36,93 +43,82 @@ void pong_movePaddle(struct paddle *paddle, uint8_t direction)
     {
         paddle->y--;
     }
+
+    ssd1306_FillRectangle(paddle->x, paddle->y, paddle->x + paddle->width, paddle->y + paddle->height, White);
 }
 
-void pong_checkForCollision(void)
+static void priv_displayScore(uint16_t score)
 {
-    if (ball.x == PONG_FIELD_X0 && (ball.y < player1.y || ball.y > (player1.y + player1.height)))
-    {
-        player1.isLost = true;
-    }
-    if (ball.x == (PONG_FIELD_X1 - 1) && (ball.y < player2.y || ball.y > (player2.y + player2.height)))
-    {
-        player2.isLost = true;
-    }
+    static char buff[20];
+
+    snprintf(buff, 20, "Score: %d   ", score);
+    ssd1306_SetCursor(PONG_SCORE_X, 0);
+    ssd1306_WriteString(buff, Font_6x8, White);
+}
+
+static priv_initGame(struct ball *ball, struct paddle *player1, struct paddle *player2)
+{
+    ssd1306_FillRectangle(player1->x, player1->y, player1->x + player1->width, player1->y + player1->height, White);
+    ssd1306_FillRectangle(player2->x, player2->y, player2->x + player2->width, player2->y + player2->height, White);
+    ssd1306_FillRectangle(ball->x, ball->y, ball->x + ball->size, ball->y + ball->size, Black);
 }
 
 void pong_gameloop(void)
 {
     while (true)
     {
+
+        struct paddle player1 = {0, 20, 4, 16};
+        struct paddle player2 = {123, 25, 4, 16};
+        struct ball ball = {0, 0, 1, 1, 1};
+        ball.x = random_randint(PONG_BALL_X0, PONG_BALL_X1);
+        ball.y = random_randint(PONG_BALL_Y0, PONG_BALL_Y1);
+
         ssd1306_Fill(Black);
-
-        char buff0[20];
-        snprintf(buff0, 20, "Score: %d   ", TimerTick);
-        ssd1306_SetCursor(42, 0);
-        ssd1306_WriteString(buff0, Font_6x8, White);
-
-        // ssd1306_DrawRectangle(PONG_FIELD_X0, PONG_FIELD_Y0, PONG_FIELD_X1, PONG_FIELD_Y1, White);
-        ssd1306_FillRectangle(player1.x, player1.y, player1.x + player1.width, player1.y + player1.height, White);
-        ssd1306_FillRectangle(player2.x, player2.y, player2.x + player2.width, player2.y + player2.height, White);
-        ssd1306_FillRectangle(ball.x, ball.y, ball.x + ball.size, ball.y + ball.size, Black);
+        priv_initGame(&ball, &player1, &player2);
         ssd1306_UpdateScreen();
+
+        uint16_t score = 0;
+        TimerTick = 0;
         while (1)
         {
-            ssd1306_FillRectangle(ball.x, ball.y, ball.x + ball.size, ball.y + ball.size, Black);
-            pong_moveBall(&ball);
-            ssd1306_FillRectangle(ball.x, ball.y, ball.x + ball.size, ball.y + ball.size, White);
-            ssd1306_UpdateScreen();
-            pong_checkForCollision();
+            score = TimerTick;
+            priv_displayScore(score);
+            priv_moveBall(&ball);
+
+            if (ball.x == PONG_FIELD_X0 && (ball.y < player1.y || ball.y > (player1.y + player1.height)))
+            {
+                player1.isLost = true;
+                break;
+            }
+            if (ball.x == (PONG_FIELD_X1 - 1) && (ball.y < player2.y || ball.y > (player2.y + player2.height)))
+            {
+                player2.isLost = true;
+                break;
+            }
 
             if (BUTTON_DOWN_IS_HOLD)
             {
-                ssd1306_FillRectangle(player2.x, player2.y, player2.x + player2.width, player2.y + player2.height, Black);
-                pong_movePaddle(&player2, Paddle_Down);
-                ssd1306_FillRectangle(player2.x, player2.y, player2.x + player2.width, player2.y + player2.height, White);
+                priv_movePaddle(&player2, Paddle_Down);
             }
-            else if (BUTTON_UP_IS_HOLD)
+            if (BUTTON_UP_IS_HOLD)
             {
-                ssd1306_FillRectangle(player2.x, player2.y, player2.x + player2.width, player2.y + player2.height, Black);
-                pong_movePaddle(&player2, Paddle_Up);
-                ssd1306_FillRectangle(player2.x, player2.y, player2.x + player2.width, player2.y + player2.height, White);
+                priv_movePaddle(&player2, Paddle_Up);
             }
 
             // Using X axis, because OLED is mirrored, Y became X
             if (joystcik_getX() < JOYSTICK_CENTER - JOYSTICK_CENTER_OFFSET)
             {
-                ssd1306_FillRectangle(player1.x, player1.y, player1.x + player1.width, player1.y + player1.height, Black);
-                pong_movePaddle(&player1, Paddle_Up);
-                ssd1306_FillRectangle(player1.x, player1.y, player1.x + player1.width, player1.y + player1.height, White);
+                priv_movePaddle(&player1, Paddle_Up);
             }
-            else if (joystcik_getX() > JOYSTICK_CENTER + JOYSTICK_CENTER_OFFSET)
+            if (joystcik_getX() > JOYSTICK_CENTER + JOYSTICK_CENTER_OFFSET)
             {
-                ssd1306_FillRectangle(player1.x, player1.y, player1.x + player1.width, player1.y + player1.height, Black);
-                pong_movePaddle(&player1, Paddle_Down);
-                ssd1306_FillRectangle(player1.x, player1.y, player1.x + player1.width, player1.y + player1.height, White);
+                priv_movePaddle(&player1, Paddle_Down);
             }
 
-            if (player1.isLost)
-            {
-                // ssd1306_SetCursor(10, 15);
-                // ssd1306_WriteString("Player 1 lost", Font_6x8, White);
-                // ssd1306_UpdateScreen();
-                // while (1)
-                //     ;
-                break;
-            }
-            if (player2.isLost)
-            {
-                // ssd1306_SetCursor(10, 15);
-                // ssd1306_WriteString("Player 2 lost", Font_6x8, White);
-                // ssd1306_UpdateScreen();
-                // while (1)
-                //     ;
-                break;
-            }
-
-            // _delay_ms(5);
+            ssd1306_UpdateScreen();
         }
-        if (game_over(1, Game_Pong)) // TODO add score
+        if (game_over(score, Game_Pong))
         {
             break;
         }
